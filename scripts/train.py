b2516a85729d17fc5a6e95464037a33a8172d541
Aysha Afrah <ayshaafrah21@gmail.com>
2024-12-05 23:33:56 +0530

    training gpt model

--- scripts/train.py
+++ scripts/train.py
@@ -1,5 +1,5 @@
 from aitextgen import aitextgen
 
 ai = aitextgen(tf_gpt2 = "124M",to_gpu=True)
-generated_text = ai.generate(prompt = "once upon a time")
-print(generated_text)
+generated_text = ai.generate(prompt="Once upon a time,", max_length=200, temperature=0.7)
+print(generated_text) 
